206796a6f9da0928f607bfaf106f282226053ebb
Junaid Ahmed <junaid7ahmed96@gmail.com>
2023-04-13 16:11:59 -0400

    created parallax logo

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import NavBar from "../components/NavBar";
+import LogoParallax from "../components/LogoParallax";
 
 const HomePage = () => {
   return (
     <>
       <NavBar />
       <h1>HOME PAGE</h1>
+      <LogoParallax />
     </>
   );
 };
