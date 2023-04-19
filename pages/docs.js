1018a24387956c1466ae77b407b43ef9c6f007e7
Junaid Ahmed <junaid7ahmed96@gmail.com>
2023-04-19 09:38:52 -0400

    deployed docs and linked docs page, changed up homepage

--- pages/docs.js
+++ pages/docs.js
@@ -1,13 +1,15 @@
-import NavBar from '../components/NavBar';
-
+import NavBar from "../components/NavBar";
 
 const DocsPage = () => {
-    return (
-      <>
-        <NavBar showFadeEffect = {false} />
-        <h1>DOC PAGE</h1>
-      </>
-    );
-  };
-  
-  export default DocsPage;
+  return (
+    <>
+      <NavBar showFadeEffect={false} />
+      <iframe
+        src="https://master--zippy-naiad-6561d5.netlify.app/docs/intro"
+        className="mt-24 w-full h-screen"
+      ></iframe>
+    </>
+  );
+};
+
+export default DocsPage;
