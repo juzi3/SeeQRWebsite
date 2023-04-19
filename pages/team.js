67eec94f0d93977d1fa85ead5c350b1bff5ee094
ChaseSizemore <ChaseLSizemore@gmail.com>
2023-04-19 09:33:01 -0400

    added github logo to assets, created teams component, edited global css and added to tailwi config

--- pages/team.js
+++ pages/team.js
@@ -1,58 +1,18 @@
-import  NavBar from  '../components/NavBar'
+import NavBar from '../components/NavBar';
+// import Image from 'next/image';
+// import img from "../assets/1_720.png"
+import Teams from '../components/Teams';
 
 const TeamPage = () => {
-    return(
-        <div>
-            <NavBar showFadeEffect = {false} />
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
-            <h1>Team Page</h1>
+  return (
+    <div className="mt-50">
+      <NavBar showFadeEffect={false} />
+      <div className='flex flex-col justify-center items-center'>
+        <h1 className="text-4xl">The Team</h1>
+        <Teams />
+      </div>
+    </div>
+  );
+};
 
-
-        </div>
-    )
-}
-
-export default TeamPage
+export default TeamPage;
